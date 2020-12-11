99326097d9c5e1b886650dd377c51067bc54eef4
Daniel Uribe <danieluribe@Daniels-MacBook-Air.local>
2020-12-11 12:35:10 -0700

    commit

--- temple/scripts/services.js
+++ temple/scripts/services.js
@@ -26,6 +26,15 @@ fetch('scripts/temples.json')
             let placeOfBirth = document.createElement('p');
             placeOfBirth.textContent = `Place: ${prophet.birthplace}`;
 
+            let address = document.createElement('p');
+            address.textContent = `Address: ${prophet.address}`;
+
+            let tel = document.createElement('p');
+            tel.textContent = `Telephone: ${prophet.tel}`;
+
+            let services = document.createElement('p');
+            services.textContent = `Services: ${prophet.services}`;
+
             let image = document.createElement('img');
             image.setAttribute('src', prophet.imageurl);
             image.setAttribute('alt', `${prophet.name} ${prophet.lastname}`);
@@ -34,6 +43,9 @@ fetch('scripts/temples.json')
             article.appendChild(h1);
             article.appendChild(dateOfBirth);
             article.appendChild(placeOfBirth);
+            article.appendChild(address);
+            article.appendChild(tel);
+            article.appendChild(services);
             article.appendChild(image);
 
             document.querySelector('#prophets').appendChild(article);
